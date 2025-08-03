6b0ad218b6d80b80f104756abf7983b563f7c9af
kamalaly611 <etopiyaaly611@gmail.com>
2025-08-03 20:05:08 +0500

    Initial commit with Docker p

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -6,10 +6,20 @@ pipeline {
         }
     }
     stages {
+        stage('Checkout') {
+            steps {
+                git 'https://github.com/nxtanalyst/MyFirstPipeline.git'
+            }
+        }
         stage('Build Docker Image') {
             steps {
                 sh 'DOCKER_CONFIG=$PWD/.docker docker build -t my-node-app .'
                 sh 'rm -rf .docker'
+            }
+        }
+        stage('Run Container') {
+            steps {
+                sh 'docker run -d -p 3000:3000 --name my-node-container my-node-app'
             }
         }
     }
