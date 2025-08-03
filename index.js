634c65cac2fd9b0f4384ae10431bddf46f2dd902
kamalaly611 <etopiyaaly611@gmail.com>
2025-08-04 03:13:33 +0500

    Initial comtp

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Dockerized Node App!');
+  res.send('Isda am good what about you!');
 });
 
 app.listen(PORT, () => {
