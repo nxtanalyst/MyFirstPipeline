7735c50959750dc074e4571e5b9011ff09f130a9
kamalaly611 <etopiyaaly611@gmail.com>
2025-08-03 20:07:30 +0500

    Initial commit with Dp

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,21 +5,25 @@ pipeline {
             args '-v /var/run/docker.sock:/var/run/docker.sock'
         }
     }
+    environment {
+        HOME = "${env.WORKSPACE}"  // ✅ Override HOME to avoid permission error
+    }
     stages {
         stage('Checkout') {
             steps {
                 git 'https://github.com/nxtanalyst/MyFirstPipeline.git'
             }
         }
+
         stage('Build Docker Image') {
             steps {
-                sh 'DOCKER_CONFIG=$PWD/.docker docker build -t my-node-app .'
-                sh 'rm -rf .docker'
+                sh 'docker build -t my-node-app .'
             }
         }
+
         stage('Run Container') {
             steps {
-                sh 'docker run -d -p 3000:3000 --name my-node-container my-node-app'
+                sh 'docker run -d -p 3000:3000 --name my-node-app my-node-app'
             }
         }
     }
